2d6d225529c0ff4bc3147c3f2eabbf383e4f8d50
CianMW <markwick.cian@gmail.com>
2021-11-24 11:00:52 +0000

    default search implemented

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {Provider} from "react-redux"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider stor={configureStore}>
+    <Provider store={configureStore}>
     <App />
     </Provider>
   </React.StrictMode>,
